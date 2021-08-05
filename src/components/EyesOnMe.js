dccf1bc8febacc5cb2e3304ed7ccf3eac5fb8702
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-05 12:33:49 -0700

    learn how to handle events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component{
+
+    // 2 ways you can add event handlers! 
+
+    blur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render(){
+        return(
+            <button onFocus={() => console.log('Good!')} onBlur={this.blur}> Eyes on me, please! </button>
+        )
+    }
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React, { Component } from 'react' 
+// import EyesOnMe from './EyesOnMe'
+
+
+
+class Keypad extends Component {
+
+    handleUp = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return (
+        <div>
+            <input onKeyUp={this.handleUp} type="password" /> 
+        </div> 
+            
+        )
+    }
+}
+
+export default Keypad
